f47a98b2f6bc9ac5f424dd4552b4893cdf5c8a18
moazam <moazamawan256@gmail.com>
2023-09-10 12:10:32 +0500

    js-alert

---
+++ app.js
@@ -0,0 +1,11 @@
+// //task one greet your user
+// alert("welcome to the website")
+// //task two
+// alert("error: please enter a valid password")
+// //task three
+// alert("welcome in the world of programming  \n now you can use them ")
+// //task four
+// alert("welcome to js land" )
+// alert("HAPPY CODING! \n prevent this page from creating additional dialogues" )
+//  //task five
+//  alert("Hello...I can run through my web browser console")
