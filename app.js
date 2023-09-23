734cd8148336e1e106643ab055e21fa0c73e58c7
moazam <moazamawan256@gmail.com>
2023-09-23 16:17:37 +0500

    all assignment

--- app.js
+++ app.js
@@ -1,11 +1,137 @@
-// //task one greet your user
-// alert("welcome to the website")
-// //task two
-// alert("error: please enter a valid password")
-// //task three
-// alert("welcome in the world of programming  \n now you can use them ")
-// //task four
-// alert("welcome to js land" )
-// alert("HAPPY CODING! \n prevent this page from creating additional dialogues" )
-//  //task five
-//  alert("Hello...I can run through my web browser console")
+// Chapter 1 (ALERTS)
+
+//task 1 greet your user
+alert("welcome to the website")
+//task 2
+alert("error: please enter a valid password")
+//task 3
+alert("welcome in the world of programming  \n now you can use them ")
+//task 4
+alert("welcome to js land" )
+alert("HAPPY CODING! \n prevent this page from creating additional dialogues" )
+ //task 5
+alert("Hello...I can run through my web browser console")
+ 
+//chapter 2 (VARIABLE FOR STRINGS)
+
+//task 1
+var username
+//task 2
+var myname = "moazam ali"
+//task 3
+var message = "Hello world"
+alert(message)
+//task 4
+alert(myname)
+var age = 15
+alert(age+"years old")
+var certificate = "Mobile and app developer"
+alert(certificate)
+//task 5
+// var pizza = "pizza"
+// alert(pizza slice )
+//task 6
+var my = "My email address is "
+var email = "anc@gmail.com"
+var sum = my+email
+alert(sum)
+//task 7
+var book = "A smarter way to learn JavaScript"
+alert(book)
+//task 8
+var script = "Yah! I acn write the HTML content through Javascript"
+document.write(script)
+// Chapter 3 VARIABLES FOR NUMBERS
+//task 1
+var age1 = "I am 18 year old"
+alert(age1)
+//task 2
+var visit = "You have visited this site N times"
+alert(visit)
+//task 3
+var birthyear = "My birth year is 2004"
+var data = "Data type of my declaredvariable is number"
+document.write(birthyear+data )
+//task 4
+var Visitorname = "Johnathan"
+var product = "T-shirt"
+var quantity = 5
+document.write(Visitorname + "ordered" + quantity + product+ "on xyz clothing store")
+//the End
+// // Chapter 4: VARIABLE NAMES: LEGAL OR ILLEGAL
+// //task 1
+// var main  var main1 var main2
+// //task 2
+// //legal names
+// var task  
+// var task1
+// var task2
+// var task3
+// var task4
+// var task5
+// //illegal names
+// var 3main
+// var !kast
+// var main task
+// var const
+// var maid-ssds
+//task 3
+var heading = "Rules For Naming Js Variables"
+var a =       "variables names can only contain ,numbers, $, _. for example : $my_1stVariable"
+var b =        "Variables must begin with a letter,$ or_. For example $name, _name or name"
+var c =        "Variable names are case SENSITIVE"
+var d =        "Variable names should not be JS Keywords"
+
+document.write( heading , a ,b, c, d)
+
+//Chapter 5: MATH EXPRESSIONS
+//task 1
+var num1 = +prompt("Enter the number for addition")
+var num2 = +prompt("Enter the number for addition")
+var add = num1+num2
+document.write(add)
+//subtraction
+
+var num1sub = +prompt("Enter the number for subtraction" )
+var num2sub = +prompt("Enter the number for subtraction")
+var add = num1sub-num2sub
+document.write(add)
+
+//multiplicaation 
+
+var num1mul = +prompt("Enter the number for multiplication")
+var num2mul = +prompt("Enter the number for multiplication")
+var add = num1mul*num2mul
+document.write(add)
+
+//modulus
+
+var num1mul = +prompt("Enter the number for modulus")
+var num2mul = +prompt("Enter the number for modulud")
+var add = num1mul*num2mul
+document.write(add)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
